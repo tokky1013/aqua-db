9f3240191f38575941ff60e590d6445034c3042c
tokky1013 <a.toshiaki1013@gmail.com>
2024-07-14 03:59:33 +0900

    Update files

--- js/funcs.js
+++ js/funcs.js
@@ -91,7 +91,7 @@ function showTable(songs) {
         <tr class="table-dark">
             <th style="width: 120px;">曲名</th>
             <th style="width: 120px;">アーティスト名</th>
-            <th style="width: 65px;">キー</th>
+            <th style="width: 80px;">キー</th>
             <th style="width: 75px;">最高音※1</th>
             <th style="width: 90px;">最高得点※2</th>
             <th>コメント</th>
